b287e0dc984950c422aa40a1d6c8cc3d2be8db1a
Colton A Palumbo <cp352@pvhs-lab112-d11.pvsd.org>
2016-09-08 14:29:29 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Declare cariable you:
+        // String or int or double
+        String s;
+        s = "name";//initialize
+        String s2 = "nickname";
+        System.out.println(s);
+        //Naming rules of variables
+        //1. if, else, void, public, static... all reserved words
+        //avoid reserved words
+        //2. Cant start it with a number
+        //EX: 3blah
+        //3. Cant be specila characters like @#$%
+        //4. Cant have spaces
+        //EX: First Name instead use lowercase one word then uppercase the other or something
+        //5. Cant reuse variable names
+        //String vs int
+        //int vs double two seperate types of data
+        int  a = 45;
+        double b = 3;
+          double sum = a + b;
+
+         a = 2342342;
+        int E,d;
+        int sometime;
+        int Thissssssssss = 0;
+        String something = "";
+        // String object pieces of it
+        // Strings have methods.
+    }
+}
